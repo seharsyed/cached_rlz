c06d632c28691c4bcbd8fcf856fd9efc11a35051
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-01-24 15:30:08 +0200

    rlz index access benchmark

---
+++ parser_bs_vec/benchmark.cpp
@@ -0,0 +1,82 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <span>
+
+#include "parser.hpp"
+#include "random_access_rlz.hpp"
+
+std::vector<std::size_t> generate_sampling_positions(const std::size_t n, const std::size_t sz) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<std::size_t> distribution(0, sz - 1);
+
+    std::vector<std::size_t> sampling_positions;
+    sampling_positions.reserve(n);
+    for (std::size_t i = 0; i < n; ++i) {
+        sampling_positions.push_back(distribution(gen));
+    }
+
+    return sampling_positions;
+}
+
+template<typename T>
+double access_1_benchmark(const random_access_rlz<T>& rrlz, const std::size_t input_sz) {
+    const auto sampling_positions = generate_sampling_positions(10'000, input_sz);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (const auto pos : sampling_positions) {
+        rrlz.access(pos);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    return duration.count();
+}
+
+template<typename T>
+double access_10_benchmark(const random_access_rlz<T>& rrlz, const std::size_t input_sz) {
+    const auto sampling_positions = generate_sampling_positions(10'000, input_sz - 9);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (const auto pos : sampling_positions) {
+        for (std::size_t i = 0; i < 10; ++i) {
+            rrlz.access(pos + i);
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    return duration.count();
+}
+
+template<typename T>
+double access_100_benchmark(const random_access_rlz<T>& rrlz, const std::size_t input_sz) {
+    const auto sampling_positions = generate_sampling_positions(10'000, input_sz - 99);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (const auto pos : sampling_positions) {
+        for (std::size_t i = 0; i < 100; ++i) {
+            rrlz.access(pos + i);
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    return duration.count();
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::fprintf(stderr, "usage: %s [reference file] [suffix array file] [input file]\n", argv[0]);
+        std::exit(1);
+    }
+
+    auto ref_vec = read_file<unsigned char>(argv[1]);
+    auto sa_vec = read_file<unsigned int>(argv[2]);
+    auto input_vec = read_file<unsigned char>(argv[3]);
+    auto res = lzFactorize<unsigned char, unsigned int>(input_vec.data(), input_vec.size(), ref_vec.data(), ref_vec.size(), sa_vec.data());
+
+    random_access_rlz<unsigned char> rrlz(ref_vec, res);
+    std::cout << "Access length 1: " << access_1_benchmark(rrlz, input_vec.size()) << " seconds\n";
+    std::cout << "Access length 10: " << access_10_benchmark(rrlz, input_vec.size()) << " seconds\n";
+    std::cout << "Access length 100: " << access_100_benchmark(rrlz, input_vec.size()) << " seconds\n";
+}
