e93e6dd0ccd3a40b1ee4d698ec5088176649634f
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-02-05 15:14:20 +0200

    make reference pointers not relative

--- parser/random_access_rlz.hpp
+++ parser/random_access_rlz.hpp
@@ -2,6 +2,7 @@
 
 #include <span>
 #include <tuple>
+#include <unordered_map>
 #include <vector>
 
 #include <cstdint>
@@ -13,16 +14,14 @@ struct random_access_rlz {
     sdsl::sd_vector<> starts;
     sdsl::rank_support_sd<> starts_rs;
     sdsl::select_support_sd<> starts_ss;
-
     std::vector<T> ref_vec;
-
-    std::vector<std::int64_t> relative_ref_ptrs;
+    std::vector<std::size_t> ref_ptrs;
 
     random_access_rlz(const std::vector<T>& ref_vec,
                       const std::vector<std::tuple<std::size_t, std::size_t, std::size_t>>& spl_vec) : ref_vec(ref_vec) {
         const auto [last_start, last_pos, last_len] = spl_vec.back();
-        const std::int64_t decompressed_sz = last_start + last_len;
-        const std::int64_t phrases = spl_vec.size();
+        const std::size_t decompressed_sz = last_start + last_len;
+        const std::size_t phrases = spl_vec.size();
 
         sdsl::sd_vector_builder starts_builder(decompressed_sz + 1, phrases + 1);
 
@@ -35,18 +34,22 @@ struct random_access_rlz {
         sdsl::util::init_support(this->starts_rs, &(this->starts));
         sdsl::util::init_support(this->starts_ss, &(this->starts));
 
-        relative_ref_ptrs.assign(spl_vec.size(), 0);
-        relative_ref_ptrs[0] = std::get<1>(spl_vec[0]);
-        for (std::int64_t i = 1; i < spl_vec.size(); ++i) {
+        std::unordered_map<T, std::size_t> mismatch_map;
+        ref_ptrs.assign(spl_vec.size(), 0);
+        for (std::size_t i = 0; i < spl_vec.size(); ++i) {
             const auto [start, pos, len] = spl_vec[i];
 
             if (len) {
-                relative_ref_ptrs[i] = static_cast<std::int64_t>(pos) - static_cast<std::int64_t>(length_cumulative_sum(i));
-            }
-            else {
+                ref_ptrs[i] = pos;
+            } else {
                 const auto mismatched_symbol = static_cast<T>(pos ^ (1ull << 63ull));
-                this->ref_vec.push_back(mismatched_symbol);
-                relative_ref_ptrs[i] = static_cast<std::int64_t>(this->ref_vec.size() - 1) - static_cast<std::int64_t>(length_cumulative_sum(i));
+                if (mismatch_map.contains(mismatched_symbol)) {
+                    ref_ptrs[i] = mismatch_map.at(mismatched_symbol);
+                } else {
+                    this->ref_vec.push_back(mismatched_symbol);
+                    ref_ptrs[i] = this->ref_vec.size() - 1;
+                    mismatch_map.insert({mismatched_symbol, this->ref_vec.size() - 1});
+                }
             }
         }
     }
@@ -72,17 +75,18 @@ struct random_access_rlz {
     }
 
     std::size_t pos_to_pos_in_ref(const std::int64_t pos) const {
-        return relative_ref_ptrs[pos_to_phrase(pos)] + pos;
+        const auto phrase = pos_to_phrase(pos);
+        return ref_ptrs[phrase] + pos - length_cumulative_sum(phrase);
     }
 
     std::int64_t length_until_phrase_end(const std::int64_t pos) const {
         const auto phrase = pos_to_phrase(pos);
-
         return length_cumulative_sum(phrase + 1) - pos;
     }
 
     T access(const std::int64_t pos) const {
-        return ref_vec[relative_ref_ptrs[pos_to_phrase(pos)] + pos];
+        const auto phrase = pos_to_phrase(pos);
+        return ref_vec[ref_ptrs[phrase] + pos - length_cumulative_sum(phrase)];
     }
 
     void get(const std::int64_t pos, const std::int64_t len, std::vector<T>& buf) const {
