1722fd2cc83b1be7a6b317daa46ba533d1d14f55
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-06-17 11:25:38 +0300

    measure dense and sparse

--- color_set_refgen/count_tree_depth_asc.cpp
+++ color_set_refgen/count_tree_depth_asc.cpp
@@ -18,6 +18,13 @@ int main(int argc, char* argv[]) {
     std::size_t ssn = 0;
     std::size_t nb = 0;
 
+    std::size_t sparse_sets = 0;
+    std::size_t sparse_elements = 0;
+    constexpr std::size_t enc_width = 16;
+
+    std::size_t dense_sets = 0;
+    std::size_t dense_elements = 0;
+
     #pragma omp parallel for schedule(dynamic, 1) shared(color_sets, root_vec, tree_depths, ssn, nb)
     for (std::size_t i = 0; i < color_sets.size(); ++i) {
         const auto& s1 = color_sets[i];
@@ -32,11 +39,26 @@ int main(int argc, char* argv[]) {
             if (std::includes(s2.begin(), s2.end(), s1.begin(), s1.end())) {
                 #pragma omp critical
                 {
-                    ++ssn;
-                    nb += s2.size();
-                    root_vec[i] = false;
-                    if (tree_depths[j] <= tree_depths[i]) {
-                        tree_depths[j] = tree_depths[i] + 1;
+
+                    const std::size_t sparse_sz = s1.size();
+                    const std::size_t dense_sz = s2.size();
+                    const std::size_t sparse_bits = sparse_sz * enc_width;
+                    const std::size_t dense_bits = dense_sz;
+
+                    if (sparse_bits <= dense_bits) {
+                        ++ssn;
+                        ++sparse_sets;
+                        sparse_elements += sparse_sz;
+                        nb += sparse_bits;
+                    } else {
+                        ++ssn;
+                        ++dense_sets;
+                        dense_elements += dense_sz;
+                        nb += dense_bits;
+                        root_vec[i] = false;
+                        if (tree_depths[j] <= tree_depths[i]) {
+                            tree_depths[j] = tree_depths[i] + 1;
+                        }
                     }
                 }
                 break;
@@ -46,6 +68,7 @@ int main(int argc, char* argv[]) {
 
     const auto max_td = *std::max_element(tree_depths.begin(), tree_depths.end());
 
+    std::size_t root_sets = 0;
     std::size_t rse = 0;
     std::int64_t dcs = 0;
     std::int64_t nt = 0;
@@ -64,7 +87,14 @@ int main(int argc, char* argv[]) {
     std::cout << "number of subsets: " << ssn << "\n";
     std::cout << "number of bits to encode subsets: " << nb << "\n";
     std::cout << "total size of root sets: " << rse << "\n";
+
     std::cout << "number of trees: " << nt << "\n";
     std::cout << "maximum tree depth: " << max_td << "\n";
     std::cout << "mean tree depth: " << mean_td << "\n";
+
+    std::cout << "sparse sets: " << sparse_sets << "\n";
+    std::cout << "sparse elements: " << sparse_elements << "\n";
+
+    std::cout << "dense sets: " << dense_sets << "\n";
+    std::cout << "dense elements: " << dense_elements << "\n";
 }
