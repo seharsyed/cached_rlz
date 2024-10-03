957199580919b6bd99235b337c0128d1f7b6785e
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-10-03 15:11:53 +0300

    stats printing

--- new_ds/stats.cpp
+++ new_ds/stats.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
         std::cout << name << ": " << space << "\n";
     }
 
-    std::cout << "dense:" << d.dense_starts.size() << ", " << d.dense_starts[d.dense_starts.size() - 1] << "\n";
+    std::cout << "dense: " << d.dense_starts.size() << ", " << d.dense_starts[d.dense_starts.size() - 1] << "\n";
     std::cout << "sparse: " << d.sparse_starts.size() << ", " << d.sparse_starts[d.sparse_starts.size() - 1] << "\n";
     std::cout << "subset: " << d.subset_starts.size() << ", " << d.subset_starts[d.subset_starts.size() - 1] << "\n";
 }
