5c8cf5a6ccf9b97c4a6a1b6a5ec93ef2a499fe9c
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-11-13 16:01:27 +0200

    sd vector

---
+++ new_ds/sd_benchmark.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <cstdint>
+
+#include "sd_ds.hpp"
+
+std::vector<std::size_t> generate_sampling_positions(const std::size_t n, const std::size_t sz) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<std::size_t> distribution(0, sz - 1);
+
+    std::vector<std::size_t> sampling_positions;
+    sampling_positions.reserve(n);
+    for (std::size_t i = 0; i < n; ++i) {
+        sampling_positions.push_back(distribution(gen));
+    }
+
+    return sampling_positions;
+}
+
+double extract_benchmark(const sd_ds& d, const std::size_t input_sz, const std::size_t n) {
+    const auto sampling_positions = generate_sampling_positions(n, input_sz);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::uint32_t x = 0;
+    for (const auto pos : sampling_positions) {
+        const auto res{d.extract(pos)};
+        x ^= res.back(); // in order to not optimize result away
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "x: " << x << "\n";
+
+    return duration.count();
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::fprintf(stderr, "usage: %s [input file] [number of accesses]\n", argv[0]);
+        std::exit(EXIT_FAILURE);
+    }
+
+    std::ifstream ifs(argv[1]);
+
+    sd_ds d;
+    d.load(ifs);
+    ifs.close();
+
+    const std::size_t accesses = std::stoull(argv[2]);
+
+    const auto duration = extract_benchmark(d, d.size(), accesses);
+
+    std::cout << "size in bytes: " << d.size_in_bytes() << "\n";
+    std::cout << accesses << " accesses took: " <<  duration << " seconds\n";
+    std::cout << "average time per access: " <<  duration / static_cast<double>(accesses) << " seconds\n";
+}
---
+++ new_ds/sd_ds.hpp
@@ -0,0 +1,264 @@
+#pragma once
+
+#include <istream>
+#include <map>
+#include <ostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#include <cstdint>
+
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/bits.hpp>
+#include <sdsl/int_vector.hpp>
+#include <sdsl/sd_vector.hpp>
+
+struct sd_ds {
+    sdsl::sd_vector<> dense_container;
+    sdsl::int_vector<> dense_starts;
+
+    sdsl::int_vector<> sparse_container;
+    sdsl::int_vector<> sparse_starts;
+
+    sdsl::bit_vector subset_container;
+    sdsl::int_vector<> subset_starts;
+    sdsl::int_vector<> parent_vec;
+
+    sd_ds() {}
+
+    // ds(const sdsl::bit_vector& dense_container,
+    //    const sdsl::int_vector<>& dense_starts,
+    //    const sdsl::int_vector<>& sparse_container,
+    //    const sdsl::int_vector<>& sparse_starts,
+    //    const sdsl::bit_vector& subset_container,
+    //    const sdsl::int_vector<>& subset_starts,
+    //    const sdsl::int_vector<>& parent_vec)
+    //     : dense_container(dense_container),
+    //       dense_starts(dense_starts),
+    //       sparse_container(sparse_container),
+    //       sparse_starts(sparse_starts),
+    //       subset_container(subset_container),
+    //       subset_starts(subset_starts),
+    //       parent_vec(parent_vec) {}
+
+    std::int64_t size_in_bytes() const {
+        return sdsl::size_in_bytes(dense_container)
+            + sdsl::size_in_bytes(dense_starts)
+            + sdsl::size_in_bytes(sparse_container)
+            + sdsl::size_in_bytes(sparse_starts)
+            + sdsl::size_in_bytes(subset_container)
+            + sdsl::size_in_bytes(subset_starts)
+            + sdsl::size_in_bytes(parent_vec);
+    }
+
+    // std::int64_t serialize(std::ostream& os) const {
+    //     std::int64_t bytes_written = 0;
+
+    //     bytes_written += dense_container.serialize(os);
+    //     bytes_written += dense_starts.serialize(os);
+
+    //     bytes_written += sparse_container.serialize(os);
+    //     bytes_written += sparse_starts.serialize(os);
+
+    //     bytes_written += subset_container.serialize(os);
+    //     bytes_written += subset_starts.serialize(os);
+    //     bytes_written += parent_vec.serialize(os);
+
+    //     return bytes_written;
+    // }
+
+    void load(std::istream& is) {
+        sdsl::bit_vector dense_temp;
+        dense_temp.load(is);
+        dense_container = sdsl::sd_vector<>(dense_temp);
+        dense_starts.load(is);
+
+        sparse_container.load(is);
+        sparse_starts.load(is);
+
+        subset_container.load(is);
+        subset_starts.load(is);
+        parent_vec.load(is);
+    };
+
+    bool is_root(const std::int64_t idx) const {
+        return idx < root_count();
+    }
+
+    bool is_subset(const std::int64_t idx) const {
+        return !is_root(idx);
+    }
+
+    bool is_dense(const std::int64_t idx) const {
+        return idx < dense_count();
+    }
+
+    bool is_sparse(const std::int64_t idx) const {
+        return is_root(idx) && !is_dense(idx);
+    }
+
+    std::int64_t dense_count() const {
+        return dense_starts.size() - 1;
+    }
+
+    std::int64_t sparse_count() const {
+        return sparse_starts.size() - 1;
+    }
+
+    std::int64_t subset_count() const {
+        return subset_starts.size() - 1;
+    }
+
+    std::int64_t root_count() const {
+        return dense_count() + sparse_count();
+    }
+
+    std::int64_t size() const {
+        return dense_count() + sparse_count() + subset_count();
+    }
+
+    std::int64_t dense_idx(const std::int64_t idx) const {
+        return idx;
+    }
+
+    std::int64_t sparse_idx(const std::int64_t idx) const {
+        return idx - dense_count();
+    }
+
+    std::int64_t subset_idx(const std::int64_t idx) const {
+        return idx - root_count();
+    }
+
+    std::vector<std::uint32_t> extract(const std::int64_t idx) const {
+        if (is_dense(idx)) {
+            return extract_dense(dense_idx(idx));
+        } else if (is_sparse(idx)) {
+            return extract_sparse(sparse_idx(idx));
+        } else {
+            return extract_subset(subset_idx(idx));
+        }
+    }
+
+    std::vector<std::uint32_t> extract_dense(const std::int64_t idx) const {
+        const std::size_t beg = dense_starts[idx];
+        const std::size_t end = dense_starts[idx + 1];
+        const std::size_t sz = end - beg;
+
+        // TODO: optimize to use popcount
+        std::size_t elems = 0;
+        for (std::size_t i = 0; i < sz; ++i) {
+            if (dense_container[beg + i]) {
+                ++elems;
+            }
+        }
+
+        std::vector<std::uint32_t> s(elems);
+        for (std::size_t i = 0, j = 0; i < sz; ++i) {
+            if (dense_container[beg + i]) {
+                s[j++] = i;
+            }
+        }
+
+        return s;
+    }
+
+    std::vector<std::uint32_t> extract_sparse(const std::int64_t idx) const {
+        const std::size_t beg = sparse_starts[idx];
+        const std::size_t end = sparse_starts[idx + 1];
+        const std::size_t sz = end - beg;
+
+        std::vector<std::uint32_t> s(sz);
+        for (std::size_t i = 0; i < sz; ++i) {
+            s[i] = sparse_container[beg + i];
+        }
+
+        return s;
+    }
+
+    std::vector<std::uint32_t> extract_subset(const std::int64_t idx) const {
+        static std::stack<std::int64_t> st;
+        st.push(idx);
+        std::int64_t parent = parent_vec[idx];
+        while (is_subset(parent)) {
+            parent = subset_idx(parent);
+            st.push(parent);
+            parent = parent_vec[parent];
+        }
+
+        std::size_t beg = 0;
+        std::size_t end = 0;
+        std::size_t sz = 0;
+
+        if (is_dense(parent)) {
+            const auto root = dense_idx(parent);
+            beg = dense_starts[root];
+            end = dense_starts[root + 1];
+            sz = end - beg;
+        } else {
+            const auto root = sparse_idx(parent);
+            beg = sparse_starts[root];
+            end = sparse_starts[root + 1];
+            sz = sparse_container[end - 1] + 1;
+        }
+
+        sdsl::bit_vector bv(sz, 0);
+
+        if (is_dense(parent)) {
+            for (std::size_t i = 0; i < (end - beg); ++i) {
+                bv[i] = dense_container[beg + i];
+            }
+        } else {
+            for (std::size_t i = 0; i < (end - beg); ++i) {
+                bv[sparse_container[beg + i]] = 1;
+            }
+        }
+
+        while (st.size()) {
+            const auto ss = st.top(); st.pop();
+            const auto ss_beg = subset_starts[ss];
+            const auto ss_end = subset_starts[ss + 1];
+            const auto words = (bv.size() + 63) / 64;
+
+            for (std::size_t w = 0, elem = ss_beg; w < words; ++w) {
+                const std::uint64_t bits = std::popcount(bv.data()[w]);
+                std::uint64_t mask = ~0ull;
+                std::uint64_t temp = 0ull;
+                for (std::uint64_t b = 1; b <= bits; ++b) {
+                    const std::uint64_t idx = std::countr_zero(bv.data()[w] & mask);
+                    const std::uint64_t bit = subset_container[elem++];
+                    temp |= (bit << idx);
+                    mask &= ~(1ull << idx);
+                }
+                bv.data()[w] = temp;
+            }
+        }
+
+        const auto words = (bv.size() + 63) / 64;
+        std::size_t elems = 0;
+        for (std::size_t w = 0; w < words; ++w) {
+            elems += std::popcount(bv.data()[w]);
+        }
+
+        std::vector<std::uint32_t> s(elems);
+        for (std::size_t i = 0, j = 0; i < bv.size(); ++i) {
+            if (bv[i]) {
+                s[j++] = i;
+            }
+        }
+
+        return s;
+    }
+
+    std::map<std::string, std::int64_t> space_breakdown() const {
+       return std::map<std::string, std::int64_t>{
+            {"dense_container", sdsl::size_in_bytes(dense_container)},
+            {"dense_starts", sdsl::size_in_bytes(dense_starts)},
+            {"sparse_container", sdsl::size_in_bytes(sparse_container)},
+            {"sparse_starts", sdsl::size_in_bytes(sparse_starts)},
+            {"subset_container", sdsl::size_in_bytes(subset_container)},
+            {"subset_starts", sdsl::size_in_bytes(subset_starts)},
+            {"parent_vec", sdsl::size_in_bytes(parent_vec)}
+        };
+    }
+};
