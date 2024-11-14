856f027007355120579d8eafb96c0d82df50e30e
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-11-14 12:19:56 +0200

    add featuers to stats

--- new_ds/stats.cpp
+++ new_ds/stats.cpp
@@ -23,4 +23,22 @@ int main(int argc, char* argv[]) {
     std::cout << "dense: " << d.dense_starts.size() << ", " << d.dense_starts[d.dense_starts.size() - 1] << "\n";
     std::cout << "sparse: " << d.sparse_starts.size() << ", " << d.sparse_starts[d.sparse_starts.size() - 1] << "\n";
     std::cout << "subset: " << d.subset_starts.size() << ", " << d.subset_starts[d.subset_starts.size() - 1] << "\n";
+
+    std::size_t colors = 0;
+    for (std::size_t i = 0; i < d.dense_container.size(); ++i) {
+        colors += d.dense_container[i];
+    }
+
+    colors += d.sparse_container.size();
+
+    for (std::size_t i = 0; i < d.subset_container.size(); ++i) {
+        colors += d.subset_container[i];
+    }
+
+    std::cout << "number of colors in sets: " << colors << "\n";
+
+    const double bits = static_cast<double>(d.size_in_bytes()) * 8.0;
+
+    const double bpc = bits / static_cast<double>(colors);
+    std::cout << "bits per color: " << bpc << "\n";
 }
