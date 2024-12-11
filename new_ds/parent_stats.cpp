6bd8c358ca4d3dbc9cfa3275825ccd16df0f55b3
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-12-11 17:50:18 +0200

    fix typo

--- new_ds/parent_stats.cpp
+++ new_ds/parent_stats.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    const std::size_t subsets_sets = sets - roots;
+    const std::size_t subsets = sets - roots;
 
     std::cout << "number of sets: " << sets << "\n";
     std::cout << "number of roots: " << root_sets << "\n";
