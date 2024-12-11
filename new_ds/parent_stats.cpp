9051bb01fd6b701c992324fac756448ff77783b3
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2024-12-11 17:49:08 +0200

    fix order

--- new_ds/parent_stats.cpp
+++ new_ds/parent_stats.cpp
@@ -11,14 +11,14 @@ int main(int argc, char* argv[]) {
 
     const auto parents = get_parents<std::int64_t>(argv[1]);
     const std::size_t sets = parents.size();
-    std::size_t subsets = 0;
+    std::size_t roots = 0;
     for (const auto p : parents) {
         if (p == -1) {
-            ++subsets;
+            ++roots;
         }
     }
 
-    const std::size_t root_sets = sets - subsets;
+    const std::size_t subsets_sets = sets - roots;
 
     std::cout << "number of sets: " << sets << "\n";
     std::cout << "number of roots: " << root_sets << "\n";
